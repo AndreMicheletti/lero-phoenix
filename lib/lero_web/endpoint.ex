0f9f34f45a117eca761cb25acb620cd85c1e1e84
AndreMicheletti <andreluizmtmicheletti@gmail.com>
2021-04-10 17:46:49 -0300

    Use production config from Heroku ENV variables and decrease socket timeout

--- lib/lero_web/endpoint.ex
+++ lib/lero_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule LeroWeb.Endpoint do
   ]
 
   socket "/socket", LeroWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
